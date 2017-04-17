6a0912c5e9b3a1d8979472a1aa0a1fd6bc0bcbbe
Yaohan Ke <yk1587@nyu.edu>
2017-04-17 14:04:58 -0400

    quality issues between columns

---
+++ Data Quality and Summary/column21_22_quality_issue.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import sys
+from csv import reader
+from pyspark import SparkContext
+
+sc = SparkContext()
+
+csvfile = sc.textFile(sys.argv[1], 1)
+
+def remove_header(itr_index, itr):
+        return iter(list(itr)[1:]) if itr_index == 0 else itr
+
+data = csvfile.mapPartitions(lambda x: reader(x)).mapPartitionsWithIndex(remove_header)
+
+columns = data.map(lambda x: (x[21],x[22])).map(lambda x: (x,1)).reduceByKey(lambda x,y: x+y)
+
+sort = columns.sortByKey()
+
+output = sort.map(lambda x: '%s\t%s\t%s' % (x[0][0],x[0][1],x[1]))
+
+output.saveAsTextFile('column21_22_quality_issue.out')
+
+sc.stop()
