57fe62ae17d14146331fa83f8a115663d01bc5d2
ws854 <ws854@nyu.edu>
2017-04-17 14:43:20 -0400

    Col0 v2

---
+++ Data Quality and Summary/column0_data_quality_v2.py
@@ -0,0 +1,42 @@
+from __future__ import print_function
+
+import sys
+from operator import add
+from csv import reader 
+from pyspark import SparkContext
+
+# CMPLNT_NUM 
+# TO CHECK IF CMPLNT_NUM is integer and unique 
+
+# make sure the header will not be included in the data 
+# source: http://stackoverflow.com/questions/24718697/pyspark-drop-rows
+def remove_header(itr_index, itr):
+ return iter(list(itr)[1:]) if itr_index == 0 else itr 
+ 
+def check(x):
+ try: 
+  int(x)
+  return "VALID"
+ except:
+  if x == "":
+   return "NULL"
+  else:
+   return "INVALID" 
+  
+if __name__ == "__main__":
+
+ sc = SparkContext()
+ 
+ lines = sc.textFile(sys.argv[1], 1)
+ line = lines.mapPartitions(lambda x:reader(x))
+ line = line.mapPartitionsWithIndex(remove_header) # remove header 
+ cmplnt = line.map(lambda x: "%s\tINTEGER\tA unique compliant number\t%s" %(x[0],check(x[0])))
+ 
+ 
+ cmplnt.saveAsTextFile("column0_data_quality_v2.out") 
+
+ sc.stop()
+
+
+
+
